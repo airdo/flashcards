bd75f7b61da9f785d80542f6885679e80c81231d
rbsx <airdo@yandex.ru>
2016-05-18 21:02:29 +0000

    adding crud. Create,Delete

--- app/controllers/cards_controller_controller.rb
+++ app/controllers/cards_controller_controller.rb
@@ -1,12 +1,32 @@
 class CardsControllerController < ApplicationController
 	def index
 		@cards = Card.all
-		render text: @cards.map { |x| 
-			"Text: #{x.original_text} - Translation: #{x.translated_text} <br/> Updated: #{x.review_date}"
-		}.join("<hr>")
 	end
 
-	def new
-		render text "BOY"
-	end
+	 def new
+	 	@card = Card.new
+	 end
+	 
+	 def create
+	 	@card = Card.create(card_params)
+
+	 	@card.save
+	 	redirect_to url:cards_controller_index_url
+	 end
+
+	 def destroy
+	 	@card = Card.find(params[:id])
+	 	@card.destroy
+
+	 	redirect_to cards_controller_index_url
+	 end
+
+
+	 private
+	 def card_params
+	 	params.require(:cards_controller).permit(:translated_text,:original_text)
+	 end
+
+
+
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,14 @@
 class Card < ActiveRecord::Base
 	before_validation :add_review_date
+	validates :translated_text, :original_text, :review_date, presence: true
 
 	def add_review_date
 		self.review_date = Time.now.strftime("%d/%m/%Y")
+	end
+
+	def text_not_equal_translation
+		if translated_text.downcase == original_text.downcase
+			errors.add(:translated_text, 'can`t be equal with original text')
+		end
 	end
 end
