c553a12b47138bfba52ae00e1cd52ea0ab407837
rbsx <airdo@yandex.ru>
2016-05-21 14:48:28 +0000

    CRUD added

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,6 +3,7 @@ class Card < ActiveRecord::Base
 
 	before_validation :add_review_date
 	validates :translated_text, :original_text, :review_date, presence: true
+	validate :text_not_equal_translation
 
 	def add_review_date
 		self.review_date = (Date.today+3).to_s
